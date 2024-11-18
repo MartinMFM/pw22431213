3dfd75b4c96979db3187e904e9be25cc666f249b
MartinMFM <martin.mf665@gmail.com>
2024-11-18 16:23:12 -0700

    Proyecto API con TypeScript 02

--- api_node_typescript_11/src/index.ts
+++ api_node_typescript_11/src/index.ts
@@ -2,15 +2,19 @@ import express from "express";
 
 // Creamos la aplicacion a traves del paquete express
 const app = express();
+// Configurar rutas para el acceso personal
+import personalRutas from "./routes/personalRutas";
 // Todo lo que regresa al usuario es tipo JSON
 app.use(express.json());
 // Puerto para escuchar la peticion del frontend
 const PUERTO = 3001;
-
-app.get("/hola", (_req, res) => {
-  let fecha = new Date().toLocaleDateString();
-  res.send("Mundo con la fecha" + fecha + " con TypeScript");
-});
+// Activar las ruta base
+app.use("/api/personal", personalRutas);
+// RUTA
+// app.get("/hola", (_req, res) => {
+//   let fecha = new Date().toLocaleDateString();
+//   res.send("Mundo con la fecha" + fecha + " con TypeScript");
+// });
 
 // Encendemos el servidor y lo ponemos en escucha
 app.listen(PUERTO, () => {
